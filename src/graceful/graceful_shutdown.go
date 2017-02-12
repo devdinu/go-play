12219fc42ba58b9759b8b277a95f9b515db39b63
Dinesh Kumar <dinesh.kumar@go-jek.com>
2017-02-12 10:08:44 +0530

    Go Release Party Slides

---
+++ src/graceful/graceful_shutdown.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Ponged...")
+	w.Write([]byte("pong"))
+}
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", PingHandler)
+	server := &http.Server{Addr: ":8888", Handler: mux}
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	go stopServerOnInterrupt(quit, server)
+	log.Println("serving requests...")
+	server.ListenAndServe()
+}
+
+// START OMIT
+func stopServerOnInterrupt(quit chan os.Signal, server *http.Server) {
+	value := <-quit
+	log.Println("Signal Received:", value)
+	server.Shutdown(context.Background()) // HL
+	os.Exit(1)
+}
+
+//END OMIT
---
+++ src/trace/trace_me.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime/trace"
+	"sync"
+)
+
+type Gopher struct {
+	id   int
+	name string
+}
+
+func (g Gopher) Say() {
+	fmt.Println("Hi, I'm gopher:", g.id, "creator:", g.name)
+}
+
+func doSomething(name string, limit int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < limit; i++ {
+		Gopher{i, name}.Say()
+	}
+}
+
+func traceProgram() {
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	f, _ := os.Create("./dumps/trace.pprof")
+	defer f.Close()
+	defer trace.Stop()
+
+	trace.Start(f)
+	someLimit := 9000
+	go doSomething("Hundred", someLimit, wg)
+	go doSomething("Fifty", someLimit/3, wg)
+
+	wg.Wait()
+}
+
+//func main() {
+//	traceProgram()
+//}
