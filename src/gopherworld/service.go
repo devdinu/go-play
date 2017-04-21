29ec2f8caef821d2d8300ae1f6472f6c208a77bc
Dinesh Kumar <dinesh.kumar@go-jek.com>
2017-04-22 01:23:39 +0530

    Handling empty gophers, and returning first gopher

--- src/gopherworld/service.go
+++ src/gopherworld/service.go
@@ -3,5 +3,8 @@ package main
 import "errors"
 
 func tallest(gophers []Gopher) (Gopher, error) {
-	return Gopher{}, errors.New("no gophers!")
+	if gophers == nil || len(gophers) == 0 {
+		return Gopher{}, errors.New("no gophers!")
+	}
+	return gophers[0], nil
 }
--- src/gopherworld/service_test.go
+++ src/gopherworld/service_test.go
@@ -6,8 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFindingTallestGopherWithNoInput(t *testing.T) {
+func TestFailureTallestGopherWithNoInput(t *testing.T) {
 	_, err := tallest(nil)
 
 	assert.EqualError(t, err, "no gophers!")
+}
+
+func TestFailureTallestGopherWithEmptySlice(t *testing.T) {
+	_, err := tallest([]Gopher{})
+
+	assert.EqualError(t, err, "no gophers!")
+}
+
+func TestShouldReturnGopherWhenOnlyOneIsGiven(t *testing.T) {
+	g := Gopher{name: "tallest", height: 1.0}
+	tg, err := tallest([]Gopher{g})
+
+	assert.NoError(t, err)
+	assert.Equal(t, g, tg)
 }
