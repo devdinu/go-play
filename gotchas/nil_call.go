4682bdb8338f1d84d50a3765b66f810ef064274c
Dinesh Kumar <dinesh.kumar@go-jek.com>
2018-03-12 09:32:36 +0530

    Fix nill call pointer

--- gotchas/nil_call.go
+++ gotchas/nil_call.go
@@ -10,8 +10,9 @@ func (g *gopher) Name() {
 	fmt.Println("My name is gopher.")
 }
 
-func CallOnNil() {
-	var g gopher
+func main() {
+	// Thought the pointer is nil, its valid to call the method on it
+	var g *gopher
 	fmt.Printf("content: %+v %+p\n", g, g)
 	g.Name()
 }
